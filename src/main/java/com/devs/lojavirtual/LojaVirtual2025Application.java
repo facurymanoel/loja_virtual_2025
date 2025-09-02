505dbbc5fa146134bd788f19f1e44e0dee2b857c
facurymanoel <mffacuri@terra.com.br>
2025-09-02 17:02:13 -0300

    commit inicial

---
+++ src/main/java/com/devs/lojavirtual/LojaVirtual2025Application.java
@@ -0,0 +1,13 @@
+package com.devs.lojavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtual2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtual2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/devs/lojavirtual/LojaVirtual2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devs.lojavirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtual2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
